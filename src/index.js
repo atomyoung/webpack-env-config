8643f60723fc7a81405ba439475ebf77546950ba
peng <young2_peng@163.com>
2016-08-16 12:15:56 +0800

    1. 初见成效

---
+++ src/index.js
@@ -0,0 +1,3 @@
+
+document.write('Today is ' + new Date())
+
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+
+var webpack = require('webpack')
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var openBrowserWebpackPlugin = require('open-browser-webpack-plugin')
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader',
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style-loader!css-loader!sass-loader'
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=8192'
+            }
+        ]
+    },
+    plugins: [
+        // 输出html文件
+        new HtmlWebpackPlugin(),
+        // 热替换
+        new webpack.HotModuleReplacementPlugin(),
+        // 打包后自动打开浏览器
+        new openBrowserWebpackPlugin({ url: 'http://localhost:8000'}),
+        // 给输出的文件头部添加注释信息
+        new webpack.BannerPlugin('This file is created by Pyang at ' + new Date())
+    ],
+    devServer: {
+        host: '0.0.0.0',
+        port: 8000,
+        contentBae: './dist',
+        historyApiFallback: true
+    }
+}
